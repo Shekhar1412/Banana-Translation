2a6883ee24db5106602a4c8fbd75467f6fe91d92
Shekhar1412 <30951818+Shekhar1412@users.noreply.github.com>
2021-09-25 10:48:56 +0530

    update: api added

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ var input = document.querySelector("#input");
 var  btn = document.querySelector("#btn")
 var out = document.querySelector("#output");
 
-var serverURL  = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text="
+var serverURL  = "https://api.funtranslations.com/translate/minion.json?text=";
+
+function errorHandler(error)
+{
+    console.log("Some error occured", error);
+    alert("Somehthings wrong with the server !!!");
+}
 
 function doFetch(){
     const inputText =input.value;
-    
+
     fetch(serverURL+ inputText ).
     then(response=> response.json()).
-    then(json=> out.value = json.contents.translated);
+    then(json=> out.innerText = json.contents.translated).
+    catch(errorHandler);
 }
 btn.addEventListener("click", doFetch);
